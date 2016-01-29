24e2704a2670adf401e5afcc7cf6937b8e964263
Stephen Coakley <me@stephencoakley.com>
2016-01-28 23:24:15 -0600

    Error reformation

--- src/error.rs
+++ src/error.rs
@@ -1,65 +1,108 @@
 use std::error;
 use std::fmt;
-use std::io;
 use std::io::Write;
 use std::process;
 
 
-#[derive(Debug)]
-pub struct Error {
-    code: RoteError,
-    description: String,
+#[macro_export]
+macro_rules! throw {
+    ($e:expr) => (return Err($e))
+}
+
+/// Syntax sugar for pattern matching against multiple results.
+#[macro_export]
+macro_rules! try {
+    {
+        $body:expr;
+        $(catch $pattern:pat => $catch:block)*
+    } => {
+        match (|| {
+            $body;
+            Ok(())
+        })() {
+            Ok(val) => val,
+            Err(err) => match err {
+                $($pattern => $catch),*
+                _ => return Err(err)
+            }
+        }
+    };
+
+    ($result:expr) => {
+        match $result {
+            Ok(val) => val,
+            Err(err) => return Err(err),
+        }
+    };
 }
 
 #[derive(Debug)]
-pub enum RoteError {
-    FileNotReadable = 1,
-    Runtime,
-    TaskNotFound,
-    Unknown,
+pub enum Error {
+    Chained(&'static error::Error),
+    FileNotReadable(String),
+    OptionMissing(String),
+    RuntimeError(String),
+    TaskNotFound(String),
 }
 
 impl Error {
-    /// Creates a new error with a given code and description.
-    pub fn new(code: RoteError, description: &str) -> Error {
-        Error {
-            code: code,
-            description: String::from(description),
+    pub fn description(&self) -> &str {
+        match *self {
+            Error::Chained(ref err) => err.description(),
+            Error::FileNotReadable(_) => "file could not be read",
+            Error::OptionMissing(ref m) => m,
+            Error::RuntimeError(ref message) => &message,
+            Error::TaskNotFound(_) => "task not found",
         }
     }
 
-    /// Creates a new error with a given description.
-    pub fn new_desc(description: &str) -> Error {
-        Self::new(RoteError::Unknown, description)
+    /// Gets the error code.
+    pub fn code(&self) -> i32 {
+        match *self {
+            Error::Chained(_) => 1,
+            Error::OptionMissing(_) => 4,
+            Error::FileNotReadable(_) => 66,
+            Error::RuntimeError(_) => 3,
+            Error::TaskNotFound(_) => 2,
+        }
     }
 
     /// Prints the error and terminates the program, exiting with the given error code.
-    pub fn die(self) {
-        match writeln!(&mut io::stderr(), "error: {}.", self) {
-            Ok(_) => {}
-            Err(e) => panic!("Unable to write to stderr: {}", e),
-        }
-
-        process::exit(self.code as i32);
+    pub fn die(&self) {
+        error!("{}", self);
+        process::exit(self.code());
     }
 }
 
-/// Implement the standard error methods.
+/// Implements display formatting for the error type.
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Error::FileNotReadable(ref file) => write!(f, "the file \"{}\" could not be read", &file),
+            Error::TaskNotFound(ref name) => write!(f, "no task or rule found for \"{}\"", &name),
+            _ => write!(f, "{}", self.description())
+        }
+    }
+}
+
+/// And finally, Rote errors are standard errors as well.
 impl error::Error for Error {
-    /// Gets the error description.
     fn description(&self) -> &str {
-        &self.description
+        (*self).description()
     }
 
     /// Gets a previous error object, if any.
     fn cause(&self) -> Option<&error::Error> {
-        None
+        match *self {
+            Error::Chained(cause) => Some(cause),
+            _ => None,
+        }
     }
 }
 
-/// Implement display formatting for the error type.
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.description)
+/// Implement casting from other error types.
+impl From<&'static error::Error> for Error {
+    fn from(err: &'static error::Error) -> Error {
+        Error::Chained(err)
     }
 }
